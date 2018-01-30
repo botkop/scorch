06483fc5a1640ef96b14734ca3f80cd5608a1a2a
Koen Dejonghe <koen@fietsoverland.com>
2018-01-30 09:04:19 +0100

    fix gradient check

--- src/test/scala/scorch/autograd/FunctionGradientSpec.scala
+++ src/test/scala/scorch/autograd/FunctionGradientSpec.scala
@@ -21,25 +21,77 @@ class FunctionGradientSpec
   "Add" should "calculate gradients" in {
     val a = Variable(ns.randn(4, 6))
     val b = Variable(ns.randn(4, 6))
+    def f(v1: Variable, v2: Variable): Variable = Add(v1, v2).forward()
+    binOpGradientCheck(f, a, b)
+  }
+
+  it should "calculate gradients with broadcasting" in {
+    val a = Variable(ns.randn(4, 6))
+    val b = Variable(ns.randn(1, 6))
     def f(a: Variable, b: Variable): Variable = Add(a, b).forward()
-    binOpGradientCheck(a, b, f)
+    binOpGradientCheck(f, a, b)
+  }
+
+  "Sub" should "calculate gradients" in {
+    val a = Variable(ns.randn(3, 4))
+    val b = Variable(ns.randn(3, 4))
+    def f(v1: Variable, v2: Variable): Variable = Sub(v1, v2).forward()
+    binOpGradientCheck(f, a, b)
+  }
+
+  it should "calculate gradients with broadcasting" in {
+    val a = Variable(ns.randn(4, 6))
+    val b = Variable(ns.randn(1, 6))
+    def f(a: Variable, b: Variable): Variable = Sub(a, b).forward()
+    binOpGradientCheck(f, a, b)
+  }
+
+  "Mul" should "calculate gradients" in {
+    val a = Variable(ns.randn(3, 4))
+    val b = Variable(ns.randn(3, 4))
+    def f(v1: Variable, v2: Variable): Variable = Mul(v1, v2).forward()
+    binOpGradientCheck(f, a, b)
+  }
+
+  it should "calculate gradients with broadcasting" in {
+    val a = Variable(ns.randn(4, 6))
+    val b = Variable(ns.randn(1, 6))
+    def f(a: Variable, b: Variable): Variable = Mul(a, b).forward()
+    binOpGradientCheck(f, a, b)
+  }
+
+  "Div" should "calculate gradients" in {
+    val a = Variable(ns.randn(3, 4))
+    val b = Variable(ns.randn(3, 4))
+    def f(v1: Variable, v2: Variable): Variable = Div(v1, v2).forward()
+    binOpGradientCheck(f, a, b)
+  }
+
+  it should "calculate gradients with broadcasting" in {
+    val a = Variable(ns.randn(4, 6))
+    val b = Variable(ns.randn(1, 6))
+    def f(a: Variable, b: Variable): Variable = Div(a, b).forward()
+    binOpGradientCheck(f, a, b)
   }
 
   "Dot" should "calculate gradients" in {
     val a = Variable(ns.randn(4, 6))
     val b = Variable(ns.randn(6, 2))
     def f(a: Variable, b: Variable): Variable = Dot(a, b).forward()
-    binOpGradientCheck(a, b, f)
+    binOpGradientCheck(f, a, b)
   }
 
-  def binOpGradientCheck(a: Variable, b: Variable, f: (Variable, Variable) => Variable): Assertion = {
+  def binOpGradientCheck(f: (Variable, Variable) => Variable,
+                         a: Variable,
+                         b: Variable): Assertion = {
 
     val out = f(a, b)
+
     val dOut = Variable(ns.randn(out.shape: _*))
     out.backward(dOut)
 
-    val da = a.grad.get.data.copy()
-    val db = a.grad.get.data.copy()
+    val da = a.grad.get.data
+    val db = b.grad.get.data
 
     def fa(t: Tensor) = f(Variable(t), b).data
     def fb(t: Tensor) = f(a, Variable(t)).data
