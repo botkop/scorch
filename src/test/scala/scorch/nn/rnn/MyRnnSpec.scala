4188d849b1903140023fd69d40951ac873e40267
Koen Dejonghe <koen@fietsoverland.com>
2018-02-13 22:18:48 +0100

    rnn

--- src/test/scala/scorch/nn/rnn/MyRnnSpec.scala
+++ src/test/scala/scorch/nn/rnn/MyRnnSpec.scala
@@ -2,12 +2,9 @@ package scorch.nn.rnn
 
 import botkop.numsca.Tensor
 import botkop.{numsca => ns}
-import com.typesafe.scalalogging.LazyLogging
-import org.nd4j.linalg.api.buffer.DataBuffer
-import org.nd4j.linalg.factory.Nd4j
 import org.scalatest.{FlatSpec, Matchers}
 import scorch.autograd.{Variable, _}
-import scorch.nn.{Module, Optimizer, SGD}
+import scorch.nn.SGD
 
 import scala.annotation.tailrec
 import scala.io.Source
@@ -15,51 +12,19 @@ import scala.util.Random
 
 class MyRnnSpec extends FlatSpec with Matchers {
 
-  Nd4j.setDataType(DataBuffer.Type.DOUBLE)
   ns.rand.setSeed(231)
-
-  abstract class MyModule(localParameters: Seq[Variable] = Nil)
-      extends LazyLogging {
-
-    /*
-    Pytorch way of solving distinction between training and test mode is by using a mutable variable.
-    Perhaps there is a better way.
-     */
-    var inTrainingMode: Boolean = false
-
-    /*
-    Sets the module in training mode.
-    This has any effect only on modules such as Dropout or BatchNorm.
-     */
-    def train(mode: Boolean = true): Unit = {
-      this.inTrainingMode = mode
-      subModules.foreach(_.train(mode))
-    }
-
-    /*
-    Sets the module in evaluation mode.
-    This has any effect only on modules such as Dropout or BatchNorm.
-     */
-    def eval(): Unit = train(false)
-
-    def forward(xs: Seq[Variable]): Seq[Variable]
-    def apply(xs: Variable*): Seq[Variable] = forward(xs)
-    def subModules: Seq[Module] = Seq.empty
-    def parameters: Seq[Variable] =
-      localParameters ++ subModules.flatMap(_.parameters())
-    def zeroGrad(): Unit =
-      parameters.flatMap(_.grad).foreach(g => g.data := 0)
-  }
 
   case class MyRnnCell(wax: Variable,
                        waa: Variable,
                        wya: Variable,
                        ba: Variable,
                        by: Variable)
-      extends MyModule(Seq(wax, waa, wya, ba, by)) {
+      extends RecurrentModule(Seq(wax, waa, wya, ba, by)) {
+
 
     val List(na, nx) = wax.shape
     val List(ny, _) = wya.shape
+    val optimizer = SGD(parameters, lr = 0.01)
 
     override def forward(xs: Seq[Variable]): Seq[Variable] = xs match {
       case Seq(xt, aPrev) =>
@@ -73,6 +38,8 @@ class MyRnnSpec extends FlatSpec with Matchers {
         .map(_.grad.get)
         .foreach(v => v.data := ns.clip(v.data, -maxValue, maxValue))
     }
+
+    def step(): Unit = optimizer.step()
   }
 
   object MyRnnCell {
@@ -109,27 +76,7 @@ class MyRnnSpec extends FlatSpec with Matchers {
   def rnnLoss(actuals: Seq[Variable], targets: Seq[Int]): Variable =
     MyRnnLoss(actuals, targets).forward()
 
-  "My Rnn" should "well dunno...step?" in {
-    val na = 5
-    val nx = 3
-    val ny = 2
-    val m = 10
-    val rnnStep = MyRnnCell(na, nx, ny)
-
-    val at0 = Variable(ns.zeros(na, m), name = Some("at0"))
-    val xt = Variable(ns.randn(nx, m))
-
-    val Seq(yt, at) = rnnStep(xt, at0)
-
-    println(yt)
-
-    val dyt = Variable(ns.randn(yt.shape: _*))
-    yt.backward(dyt)
-
-    println(at.grad)
-  }
-
-  it should "generate dinosaur names" in {
+  "My rnn" should "generate dinosaur names" in {
 
     val data = Source
       .fromFile("src/test/resources/dinos.txt")
@@ -207,14 +154,13 @@ class MyRnnSpec extends FlatSpec with Matchers {
     def optimize(xs: List[Int],
                  ys: List[Int],
                  aPrev: Variable,
-                 rnn: MyRnnCell,
-                 optimizer: Optimizer): (Double, Variable) = {
+                 rnn: MyRnnCell): (Double, Variable) = {
       rnn.zeroGrad()
       val (yHat, a) = rnnForward(xs, aPrev, rnn)
       val loss = rnnLoss(yHat, ys)
       loss.backward()
       rnn.clipGradients(5)
-      optimizer.step()
+      rnn.step()
       (loss.data.squeeze(), a)
     }
 
@@ -227,7 +173,6 @@ class MyRnnSpec extends FlatSpec with Matchers {
               vocabSize: Int = 27): Unit = {
       val (nx, ny) = (vocabSize, vocabSize)
       val rnn = MyRnnCell(na, nx, ny)
-      val optimizer = SGD(rnn.parameters, lr = 0.01)
 
       val aPrev = Variable(ns.zeros(na, 1))
 
@@ -238,7 +183,7 @@ class MyRnnSpec extends FlatSpec with Matchers {
         val xs: List[Int] = BolIndex +: examples(index).map(charToIx).toList
         val ys: List[Int] = xs.tail :+ EolIndex
 
-        val (loss, ap) = optimize(xs, ys, aPrev, rnn, optimizer)
+        val (loss, ap) = optimize(xs, ys, aPrev, rnn)
         totalLoss += loss
         aPrev.data := ap.data // seems to have little or no effect. Why?
 
