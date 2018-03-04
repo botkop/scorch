51218e570e511a355dfedc4e3a603174c0be1e94
Koen Dejonghe <koen@fietsoverland.com>
2018-03-04 22:16:59 +0100

    rnn

--- src/main/scala/scorch/sandbox/rnn/LanguageModel.scala
+++ src/main/scala/scorch/sandbox/rnn/LanguageModel.scala
@@ -7,16 +7,17 @@ import scorch.nn.rnn.RnnBase
 import scorch.optim.{Adam, Nesterov, Optimizer, SGD}
 
 import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Random
 
 class LanguageModel[T](
     corpus: Seq[String],
     tokenize: (String) => Seq[T],
     join: Seq[T] => String,
-    encoder: (Int) => Variable,
     eosSymbol: T,
     cellType: String = "gru",
     optimizerType: String = "adam",
-    learningRate: Double = 0.06,
+    learningRate: Double = 0.001,
     na: Int = 50,
     numIterations: Int = Int.MaxValue,
     maxSentenceSize: Int = 60,
@@ -24,8 +25,9 @@ class LanguageModel[T](
     printEvery: Int = 100,
 ) {
 
+  // todo add sorting
   val tokens: Seq[T] =
-    tokenize(corpus.mkString(" ")).distinct.sorted :+ eosSymbol
+    corpus.flatMap(tokenize).distinct :+ eosSymbol
 
   val vocabSize: Int = tokens.length
   val tokenToIdx: Map[T, Int] = tokens.zipWithIndex.toMap
@@ -46,7 +48,7 @@ class LanguageModel[T](
 
   def optimize(xs: Seq[Int], ys: Seq[Int]): Double = {
     optimizer.zeroGrad()
-    val yHat = rnn.forward(xs.map(encoder))
+    val yHat = rnn.forward(xs.map(encode))
     val loss = crossEntropyLoss(yHat, ys)
     loss.backward()
     optimizer.step()
@@ -66,25 +68,31 @@ class LanguageModel[T](
 
       if (j % printEvery == 0) {
         println(s"Iteration: $j, Loss: ${totalLoss / printEvery}")
-        (1 to numSentences).foreach(_ => println(sample()))
+        (1 to numSentences).foreach(_ => println(sample))
         println()
         totalLoss = 0.0
       }
     }
   }
 
-  def sample(): String = {
+  def encode(x: Int): Variable = {
+    val xt = Variable(ns.zeros(vocabSize, 1))
+    if (x != bosIndex)
+      xt.data(x, 0) := 1
+    xt
+  }
 
-    def generateNextToken(
-        xPrev: Variable,
-        pPrev: Seq[Variable]): (Variable, Int, Seq[Variable]) = {
+  def sample: String = {
+
+    def generateToken(xPrev: Variable,
+                      pPrev: Seq[Variable]): (Variable, Int, Seq[Variable]) = {
       // Forward propagate x
       val next = rnn(xPrev +: pPrev: _*)
       val (yHat, pNext) = (next.head, next.tail)
       // Sample the index of a token within the vocabulary from the probability distribution y
       val nextIdx = ns.choice(ns.arange(vocabSize), yHat.data).squeeze().toInt
       // encoding of the next index
-      val xNext = encoder(nextIdx)
+      val xNext = encode(nextIdx)
       (xNext, nextIdx, pNext)
     }
 
@@ -98,12 +106,32 @@ class LanguageModel[T](
       } else if (t >= maxSentenceSize) {
         indices :+ eosIndex
       } else {
-        val (nextX, nextIdx, nextP) = generateNextToken(prevX, prev)
+        val (nextX, nextIdx, nextP) = generateToken(prevX, prev)
         generateSequence(t + 1, nextX, nextP, indices :+ nextIdx)
       }
 
     val sampledIndices = generateSequence()
     join(sampledIndices.init.map(tokens))
   }
+
+}
+
+object CharModel extends App {
+
+  def tokenize(s: String): Array[Char] = s.toCharArray
+  def join(s: Seq[Char]) = s.toString
+  val fileName = "src/test/resources/dinos.txt"
+  val corpus = Random.shuffle(
+    Source
+      .fromFile(fileName)
+      .getLines()
+      .toList)
+
+  val model = new LanguageModel(corpus = corpus,
+                                tokenize = tokenize,
+                                join = join,
+                                eosSymbol = '\n')
+
+  model.run()
 
 }
