20cd7ccc9608f1c2cc0e6b6330c55aa975c1821f
Koen Dejonghe <koen@fietsoverland.com>
2018-12-27 11:14:03 +0100

    add plugins oss

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
-  lazy val numsca = "be.botkop" %% "numsca" % "0.1.5-SNAPSHOT"
+  lazy val numsca = "be.botkop" %% "numsca" % "0.1.5"
 }
